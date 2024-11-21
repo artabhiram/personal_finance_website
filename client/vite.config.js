3fe224f712991a807b62401883e297e5ff43e645
artabhiram <137099699+artabhiram@users.noreply.github.com>
2024-11-21 12:02:24 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's assigned port or fallback to 5173
+    strictPort: true,              // Ensure only the specified port is used
+  },
+  preview: {
+    port: process.env.PORT || 5173, // Same for the preview server
+    strictPort: true,
+  },
 })
